76c012d494dc0c04a00d2aadcad174d5d22ee0e6
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-12-14 21:13:54 +0100

    Ajout de dossier css et img plus header footer et ma page en php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<div class="well" style="margin-bottom: 0">
+		Copyright AlTran
+	</div>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Site SISTER</title>
+</head>
+<header id="header">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Sister</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a id="navtop" href="#">Accueil</a></li>
+        <li><a id="navtop" href="#">Découvrez-Sister</a></li>
+        <li><a id="navtop" href="#">A propos</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
+<body>
