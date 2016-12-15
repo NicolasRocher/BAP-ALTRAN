4f354882bf4ada2269dae11a2b5ddff1ecdb3cad
NicolasRocher <rochernicolas97@orange.fr>
2016-12-15 11:26:55 +0100

    ajout d'images propos.php

--- propos.php
+++ propos.php
@@ -11,11 +11,11 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/picto1.png" class="img-circle" height="150px">
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </div>
         <div class="col-md-6 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/picto2.png" class="img-circle" height="150px">
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </div>
       </div>
@@ -24,7 +24,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/picto3.png" class="img-circle" height="150px">
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </div>
       </div>
@@ -36,7 +36,7 @@
       <div class="row">
         <div class="col-md-12 text-center">
           <h2>Cibles</h2>
-          <img src="http://placehold.it/250x250" class="img-circle">
+          <img src="img/cible.png" height="350px">
         </div>
       </div>
     </div>
@@ -53,12 +53,12 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/target.png" class="img-circle" height="150px">
           <h3>Objectif 1</h3>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div class="col-md-6 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/target.png" class="img-circle" height="150px">
           <h3>Objectif 2</h3>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
@@ -77,17 +77,17 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/enjeux.png" class="img-circle" height="150px">
           <h2>Enjeux 1</h2>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div class="col-md-4 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/enjeux.png" class="img-circle" height="150px">
           <h2>Enjeux 2</h2>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div class="col-md-4 text-center">
-          <img src="http://placehold.it/150x150" class="img-circle">
+          <img src="img/enjeux.png" class="img-circle" height="150px">
           <h2>Enjeux 3</h2>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
