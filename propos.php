c208bf04671e8a2aa81056e8e3b9a04c2571a2b6
NicolasRocher <rochernicolas97@orange.fr>
2017-01-03 12:14:34 +0100

    Timeline a propos

--- propos.php
+++ propos.php
@@ -1,72 +1,81 @@
 <?php include "header.php" ?>
 <section class="intro">
-    <div class="container">
-        <h1>Vertical Timeline &darr;</h1>
+  <div class="container">
+    <div class="row">
+    <h1 class"col-md-12 text-center">Domaine d'application de l'hydrogène</h1>
+  </div>
+
+  <div class="container image">
+    <div class="row">
+      <img class="col-md-4" src="img/picto1.png">
+      <img class="col-md-4" src="img/picto2.png">
+      <img class="col-md-4"src="img/picto3.png">
     </div>
+  </div>
 </section>
 
 <section class="timeline">
-    <ul>
-        <li>
-            <div>
-                <time>1934</time> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1937</time> Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1940</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1943</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1946</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1956</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1957</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1967</time> Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus, euismod quis ante.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1977</time> Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quam dolor convallis enim, non dignissim ligula ipsum a turpis.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>1985</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>2000</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-        <li>
-            <div>
-                <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-            </div>
-        </li>
-    </ul>
+  <ul>
+    <li>
+      <div>
+        <time>1934</time> De nos jours l’hydrogène est encore peu répandu en tant que source d’énergie avec 56,6 millions de tonnes utilisé par an dans le monde, soit moins de 2% de la consommation mondiale d’énergie !
+    </li>
+    <li>
+      <div>
+        <time>1937</time>Il s’utilise principalement comme matière de base à la production d’ammoniaque et de méthanol ainsi que pour le raffinage des produits pétroliers, carburants et biocarburants.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1940</time> Associé aux piles à combustibles, l’hydrogène devrait devenir un des vecteurs d’énergie les plus utilisés pour les transports et la production d ‘électricité. De plus lorsqu’il est issue de source renouvelable sa consommation ne produit que de la vapeur d’eau !
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1943</time>La consommation à partir d’énergies fossile produirait cependant d’importantes quantités de CO2, mais resterait, en prenant en compte le stockage de ce CO2, la solution la plus économique et représenterait un nouveau pas vers l’indépendance face à l’énergie nucléaire !
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1946</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1956</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1957</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1967</time> Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus, euismod quis ante.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1977</time> Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quam dolor convallis enim, non dignissim ligula ipsum a turpis.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1985</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>2000</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+      </div>
+    </li>
+  </ul>
 </section>
+
 <?php include "footer.php" ?>
