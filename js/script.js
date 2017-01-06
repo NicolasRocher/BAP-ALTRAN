e8d68a8eccdea5f96e1ba178dba7a0bcd0891ef3
Fabien Scalabrin <fscalabrin2@gmail.com>
2017-01-06 10:50:51 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -41,24 +41,6 @@ $(document).ready(function(){
     });
 });
 
-
-
-/* code pour la page projet 
-
-function bascule(){
-    var etat = document.getElementById('divprohide').style.display;
-    var bouton = document.getElementById('afficheCache');
-
-    if (etat == 'none'){
-        document.getElementById('divprohide').style.display='block';
-        bouton.value = 'Cacher la balise div';
-    }
-    else{
-        document.getElementById('divprohide').style.display='none';
-        bouton.value = 'Afficher la balise div';
-    }
-<<<<<<< HEAD
-}*/
 =======
 }
 var menu = document.querySelector('.nav__list');
