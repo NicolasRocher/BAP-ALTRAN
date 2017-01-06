cc0d00b1c7e038554ebe4de0bf88e819ac9f60f4
Fabien Scalabrin <fscalabrin2@gmail.com>
2017-01-06 17:39:48 +0100

    fleche page projet

--- projet.php
+++ projet.php
@@ -10,6 +10,8 @@
                 <a class="link link--takiri" href="#">Sister<span>Un projet d'avenir</span></a>
             </div>
         </div>
+        <div class="arrow arrow-1"></div>
+        <div class="arrow arrow-2"></div>
     </div>
 </div>
 
@@ -35,12 +37,13 @@
          </div>
          <div class="col-md-6">
          <div class="reveal">
-            <h2>Petite description du contexte</h2>
-            <p>Dans un contexte de réduction des émissions de gaz à effet de serre (plan « climat Energie), d’indépendance énergétique, de forte volatilité des matières premières sur les marchés financiers et d’incertitudes géopolitiques, le projet SISTER propose de contribuer au développement de l’hydrogène en tant que ressource par l’émergence de nouvelles filières industrielles de manière performante, sûre et économiquement compétitive. C’est dans cette perspective que le département de recherche, Altran Research, a initié en janvier 2016 un nouveau projet, SISTER, portant spécifiquement sur le stockage de l’hydrogène. Ce projet SISTER est rattaché au programme stratégique SMART ENERGIES visant à rendre plus intelligent nos usages et solutions énergétiques.</p>
+            <h2 class="colortextpro">Petite description du contexte</h2>
+            <p class="colortextpro">Dans un contexte de réduction des émissions de gaz à effet de serre (plan « climat Energie), d’indépendance énergétique, de forte volatilité des matières premières sur les marchés financiers et d’incertitudes géopolitiques, le projet SISTER propose de contribuer au développement de l’hydrogène en tant que ressource par l’émergence de nouvelles filières industrielles de manière performante, sûre et économiquement compétitive. C’est dans cette perspective que le département de recherche, Altran Research, a initié en janvier 2016 un nouveau projet, SISTER, portant spécifiquement sur le stockage de l’hydrogène. Ce projet SISTER est rattaché au programme stratégique SMART ENERGIES visant à rendre plus intelligent nos usages et solutions énergétiques.</p>
             <div class="retour"><a class="button">Retour à la normale !</a></div>
          </div>
          </div>
   </div>
+
     <div class="col-md-12" style="background-image: url('img/fondpro3.jpeg'); background-size: cover;background-attachment: fixed;">
          <div class="col-md-6 bonjour2">
          <div  class="tilt2"><a class="button">Clique moi</a></div>
@@ -48,8 +51,8 @@
          </div>
          <div class="col-md-6">
          <div class="reveal2">
-            <h2>Petite description de la technique</h2>
-            <p>L’hydrogène est une ressource discutable d’un point de vue énergétique, de masse très faible, son pouvoir calorifique volumique et molaire est faible au regard des hydrocarbures conventionnels mais il présente l’énorme avantage de n’émettre que de l’eau lors de son oxydation. A masse constante, la quantité d’énergie récupérable à partir de l’hydrogène présente cette fois-ci un avantage. Toutefois, l’hydrogène se retrouve largement pénalisé par l’encombrement et la masse de son contenant de stockage (bonbonne de gaz, vase dewar, etc.). De fait, pour produire autant d’énergie qu’1 kg d’essence, il faudrait pressurer environ 330 g d’hydrogène à 700 bar (d’après IFP Energies Nouvelles, [1]). Ceci nous met devant le défi de parvenir à trouver une solution efficace, peu coûteuse, sûre et d’un poids raisonnable pour stocker cet hydrogène.</p>
+            <h2 class="colortextpro2">Petite description de la technique</h2>
+            <p class="colortextpro2">L’hydrogène est une ressource discutable d’un point de vue énergétique, de masse très faible, son pouvoir calorifique volumique et molaire est faible au regard des hydrocarbures conventionnels mais il présente l’énorme avantage de n’émettre que de l’eau lors de son oxydation. A masse constante, la quantité d’énergie récupérable à partir de l’hydrogène présente cette fois-ci un avantage. Toutefois, l’hydrogène se retrouve largement pénalisé par l’encombrement et la masse de son contenant de stockage (bonbonne de gaz, vase dewar, etc.). De fait, pour produire autant d’énergie qu’1 kg d’essence, il faudrait pressurer environ 330 g d’hydrogène à 700 bar (d’après IFP Energies Nouvelles, [1]). Ceci nous met devant le défi de parvenir à trouver une solution efficace, peu coûteuse, sûre et d’un poids raisonnable pour stocker cet hydrogène.</p>
             <div class="retour2"><a class="button">Retour à la normale !</a></div>
          </div>
          </div>
