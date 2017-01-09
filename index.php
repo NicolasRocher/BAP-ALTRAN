9073cee4cc80923602c30090c74bc26cd733e9f6
Unknown <greenwaets@gmail.com>
2017-01-09 09:50:40 +0100

    ménage 1

--- index.php
+++ index.php
@@ -7,14 +7,6 @@
             <figcaption>BAP Altran, projet sister</figcaption>
         </figure>
         <figure>
-            <img src="http://wallpaper21.com/wp-content/uploads/2016/04/autumn_bench-HD.jpg" alt>
-            <figcaption>BAP Altran, projet sister</figcaption>
-        </figure>
-        <figure>
-            <img src="https://static.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg" alt>
-            <figcaption>BAP Altran, projet sisters</figcaption>
-        </figure>
-        <figure>
             <img src="http://www.wallpapereast.com/static/images/1321922_STpOMHj.jpg" alt>
             <figcaption>BAP Altran, projet sister</figcaption>
         </figure>
