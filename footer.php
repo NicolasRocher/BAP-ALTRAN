b64e6fbcd949d2e223048a02bef23111c21bd5dd
Unknown <greenwaets@gmail.com>
2017-01-09 07:45:11 +0100

    update footer and deleted useless things

--- footer.php
+++ footer.php
@@ -13,18 +13,21 @@
         </div>
         <div>
             <i class="fa fa-envelope"></i>
-            <p><a href="contact@adopteunpoivrot.fr">altran@gmail.com</a></p>
+            <p>Nous contacter : <br><a href="">altran@gmail.com</a></p>
         </div>
     </div>
     <div class="footer-right">
         <p class="footer-company-about">
-            <span>A propos de nous :</span>
-            Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus
-            vehicula sit amet.
+            <span>A propos d'Altran:</span>
+            Leader mondial du conseil en innovation et ingénierie avancée, Altran accompagne les entreprises dans leurs processus de création et développement de nouveaux produits et services.
         </p>
-        <div class="footer-icons">
+        <br>
+        <div class="footer-icones">
             <a href="#"><i class="fa fa-facebook"></i></a>
             <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-youtube"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
         </div>
     </div>
 </footer>
